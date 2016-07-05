e02b585861401b4f7b9d9a769c485875b12a5d79
joaopaulocosta <joaopaulomc14@hotmail.com>
2016-07-05 19:43:39 -0300

    versao1.0

---
+++ A.java
@@ -0,0 +1,21 @@
+class A{
+	private int A1;
+	private float A2;
+	
+	public void setA1(int a1){
+		this.A1 = a1;
+	}
+	
+	public int getA1(){
+		return this.A1;
+	}
+	
+	public void setA2(float a2){
+		this.A2 = a2;
+	}
+	
+	public float getA2(){
+		return this.A2;
+	}
+	
+}
