ff36d7b6aef2eb20be66ddce417919223f44be04
joaopaulocosta <joaopaulomc14@hotmail.com>
2016-07-05 20:36:33 -0300

    versao1.3

---
+++ B.java
@@ -0,0 +1,31 @@
+class B{
+	private int B1;
+	private float B2;
+	private String B4;
+	
+	
+	public void setB1(int b1){
+		this.B1 = b1;
+	}
+	
+	public int getB1(){
+		return this.B1;
+	}
+	
+	public void setB2(float b2){
+		this.B2 = b2;
+	}
+	
+	public float getB2(){
+		return this.B2;
+	}
+	
+	public void setB4(String b4){
+		this.B4 = b4;
+	}
+	
+	public String getB4(){
+		return this.B4;
+	}
+	
+}
